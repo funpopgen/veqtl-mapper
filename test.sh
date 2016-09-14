977bfc24ab819a10d696f93654a4a525cb43660f
Andrew Brown <aabrown24@hotmail.com>
2016-09-15 00:16:51 +0200

    added bash scripts for testing

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $( ./bin/VEQM --bed data/phenotype.bed --job-number 1 --genes 10 --vcf data/genotype_veqm.vcf.gz --perm 10000,4 | sha1sum | awk {'print toupper($1)'}) == "166F762354D571A6E44DDC7C04318C5FE9B11866" ]]; then
+    echo "Passed: variance test."
+else
+    echo "Failed: variance test."
+    exit 1
+fi
+
+if [[ $( ./bin/VEQM --het --bed data/phenotype.bed --job-number 1 --genes 10 --vcf data/genotype_veqm.vcf.gz --perm 10000,4 | sha1sum | awk {'print toupper($1)'}) == "5317CB6C7EDE41C8B06986FF039B5DBA369859F3" ]]; then
+    echo "Passed: heterozygote test.."
+else
+    echo "Failed: heterozygote test.."
+    exit 1
+fi
+
+echo "All tests completed successfully."

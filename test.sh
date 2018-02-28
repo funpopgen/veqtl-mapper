090e0f7c6bcb4e2fc4f77f247fa2d6105a813719
Andrew Brown <aabrown24@hotmail.com>
2018-02-28 18:52:10 +0100

    added tests to shell script

--- test.sh
+++ test.sh
@@ -20,4 +20,25 @@ else
     exit 1
 fi
 
+if [[ $( ./bin/veqtl-mapper --bed data/phenotype.bed --job-number 1 --genes 10 --vcf data/genotype.vcf.gz --perm 10000,4 --eqtl data/eQTL | sha1sum | awk {'print toupper($1)'}) == "FB61D77C8BFAFEBEDAA7DE48921BCDDD50068901" ]]; then
+    echo "Passed: eQTL test."
+else
+    echo "Failed: eQTL test."
+    exit 1
+fi
+
+if [[ $( ./bin/veqtl-mapper --bed data/phenotype.bed --job-number 1 --genes 10 --vcf data/genotype.vcf.gz --perm 10000,4 --eqtl data/eQTL --cov data/cov | sha1sum | awk {'print toupper($1)'}) == "5BE0E57D82CFA2B29319B12A89257D192D9150DD" ]]; then
+    echo "Passed: single covariate test."
+else
+    echo "Failed: single covariate test."
+    exit 1
+fi
+
+if [[ $( ./bin/veqtl-mapper --bed data/phenotype.bed --job-number 1 --genes 10 --vcf data/genotype.vcf.gz --perm 10000,4 --eqtl data/eQTL --cov data/cov_full | sha1sum | awk {'print toupper($1)'}) == "59C4BAF6199F5B015B13C710A63DC6AA9DA850CE" ]]; then
+    echo "Passed: multiple covariates test."
+else
+    echo "Failed: multiple covariates test."
+    exit 1
+fi
+
 echo "All tests completed successfully."
